83df7ab37b204e92845547c479d19c228c2d7345
Shantanu <shantanuraj@ovi.com>
2015-01-18 00:48:33 +0530

    Remove SwipeLayout from xml.

--- app/src/main/java/io/sixth/imgur9000/view/DetailActivity.java
+++ app/src/main/java/io/sixth/imgur9000/view/DetailActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
