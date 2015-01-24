4f8f7a7ed12ac61b1c295c1117bb6e209a1575b0
Shantanu <shantanuraj@ovi.com>
2015-01-25 01:04:15 +0530

    Use SwipeRefeshLayout, reload default gallery on pull.

--- app/src/main/java/io/sixth/imgur9000/view/HeroActivity.java
+++ app/src/main/java/io/sixth/imgur9000/view/HeroActivity.java
@@ -1,7 +1,6 @@
 package io.sixth.imgur9000.view;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
--- app/src/main/java/io/sixth/imgur9000/view/HeroFragment.java
+++ app/src/main/java/io/sixth/imgur9000/view/HeroFragment.java
@@ -2,11 +2,13 @@ package io.sixth.imgur9000.view;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
@@ -16,6 +18,7 @@ import java.util.ArrayList;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import io.sixth.imgur9000.R;
+import io.sixth.imgur9000.api.Imgur;
 import io.sixth.imgur9000.api.ImgurData;
 import io.sixth.imgur9000.util.App;
 import io.sixth.imgur9000.util.BusProvider;
@@ -23,7 +26,7 @@ import io.sixth.imgur9000.util.BusProvider;
 /**
  * Created by walle on 04/01/15.
  */
-public class HeroFragment extends Fragment {
+public class HeroFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
 
     private Bus bus;
 
@@ -35,7 +38,10 @@ public class HeroFragment extends Fragment {
     }
 
     @InjectView(R.id.heroRecyclerView)
-    RecyclerView mRecyclerView;
+    protected RecyclerView mRecyclerView;
+
+    @InjectView(R.id.swipeRefreshLayout)
+    protected SwipeRefreshLayout mRefreshLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -46,13 +52,19 @@ public class HeroFragment extends Fragment {
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new GridLayoutManager(App.getAppContext(), 1);
         mRecyclerView.setLayoutManager(mLayoutManager);
+        mRefreshLayout.setOnRefreshListener(this);
         return rootView;
     }
 
     @Subscribe
     public void displayResponse(ArrayList<ImgurData> images) {
+        mRefreshLayout.setRefreshing(false);
         mAdapter = new GalleryAdapter(images);
         mRecyclerView.setAdapter(mAdapter);
     }
 
+    @Override
+    public void onRefresh() {
+        Imgur.loadDefaultGallery();
+    }
 }
